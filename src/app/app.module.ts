73cf0c356ee8b6e5f6c6b4912052a768057b1f45
Vishwas Chaugule <Vishwas.Chaugule@capita.co.uk>
2021-06-27 10:59:03 +0530

    add p-carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { InputTextModule } from 'primeng/inputtext';
+import {CarouselModule} from 'primeng/carousel';
 
 const routes: Routes = [{ path: '', component: HomeComponent },
                 { path: 'movies', component: MovieDetailsComponent }];
@@ -22,12 +24,14 @@ const routes: Routes = [{ path: '', component: HomeComponent },
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     TableModule,
     RatingModule,
     FormsModule,
     RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     HttpClientModule,
-    InputTextModule  
+    InputTextModule,
+    CarouselModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
